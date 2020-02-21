662c3129320c78ffc1e3922d364ef47572e4733f
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2020-02-21 14:18:15 +0400

    graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      definitions: {
+        path: join(process.cwd(), '/src/graphql.schema.d.ts'),
+        outputAs: 'class',
+      },
+      typePaths: ['./**/*.graphql'],
+      resolverValidationOptions: {
+        requireResolversForResolveType: true,
+      },
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/messages/messages.resolver.ts
@@ -0,0 +1,24 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+
+@Resolver()
+export class MessagesResolver {
+  messagesThatReallyShouldBeInADb = [
+    {
+      id: 0,
+      description: 'The seed message',
+    },
+  ];
+
+  @Query()
+  messages() {
+    return this.messagesThatReallyShouldBeInADb;
+  }
+
+  @Mutation()
+  createMessage(@Args('description') description: string) {
+    const id = this.messagesThatReallyShouldBeInADb.length;
+    const newMessage = { id, description };
+    this.messagesThatReallyShouldBeInADb.push(newMessage);
+    return newMessage;
+  }
+}
